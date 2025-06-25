783b05a41001af5dea5878b748f8ac89a0604662
CSJ <tmdwns9630@gmail.com>
2025-06-25 23:36:00 +0900

    5-2 state 버튼 만들기

--- src/5챕터-클래스형 컴포넌트/App.js
+++ src/5챕터-클래스형 컴포넌트/App.js
@@ -24,13 +24,7 @@ class App extends React.Component {
     }));
   };
 
-  constructor(props) {
-    super(props);
-    console.log("hello");
-  }
-
   render() {
-    console.log("render time");
     return (
       <div>
         <h1>나는 클래스 컴포넌트야</h1>
